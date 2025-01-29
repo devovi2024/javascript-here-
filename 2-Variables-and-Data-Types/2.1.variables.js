5534fc7ecafd7cf58ee593f683109c9960f4c2e9
devovi2024 <arfanhosenovi@gmail.com>
2025-01-30 00:31:21 +0600

    variables

---
+++ 2-Variables-and-Data-Types/2.1.variables.js
@@ -0,0 +1,10 @@
+// Declaring variables using 'let', 'const', and 'var'
+
+
+// let(Block Scoped)
+let name = "Ovi";
+let age = 25;
+let isStudent = true;
+let hobbies = ["Coding", "Reading", "Secret"];
+let score = 95.7; //secret
+console.log(name, hobbies,score)
